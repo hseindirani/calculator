bd21d7fe1008f5f1cb4cd6bd65021bff5ebf272c
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-04-18 11:26:26 +0200

    responsive

--- src/components/bmiForm.js
+++ src/components/bmiForm.js
@@ -12,28 +12,39 @@ export default function BmiForm() {
     }
     return (
         <div>
-            <div className="flex justify-between items-center mt-14">
+            <div className="flex justify-between items-center mt-14 flex-wrap">
                 <div className="sm:w-1/2 w-full">
                     <div className="flex items-center text-2xl font-black">
-                        <input onChange={(e) => { setHeight(e.target.value) }} className="text-2xl bg-transparent border-2 p-2 mr-3" placeholder="Enter Your Height"></input>
+                        <input onChange={(e) => { setHeight(e.target.value) }} className="sm:w-[75%] w-fit sm:text-2xl text-base bg-transparent border-2 p-2 mr-3" placeholder="Enter Your Height"></input>
                         cm
                     </div>
 
                     <div className="flex items-center text-2xl font-black mt-5">
-                        <input onChange={(e) => { setWeight(e.target.value) }} className="text-2xl bg-transparent border-2 p-2 mr-3" placeholder="Enter Your Weight"></input>
+                        <input onChange={(e) => { setWeight(e.target.value) }} className="sm:w-[75%] w-fit sm:text-2xl text-base bg-transparent border-2 p-2 mr-3" placeholder="Enter Your Weight"></input>
                         kg
                     </div>
+
                 </div>
+
+                <div className="sm:hidden flex justify-center sm:w-1/2 w-full">
+                    <button className="mt-5 rounded p-2 border-2 border-black hover:bg-black hover:text-white font-semibold" onClick={() => { submit() }}>Calculate</button>
+
+                </div>
+
                 {
-                    bmi ? 
-                        <div className="sm:w-[45%] w-full text-center text-4xl font-bold">
+                    bmi ?
+                        <div className="sm:w-[45%] w-full text-center text-4xl font-bold sm:mt-0 mt-16">
                             Your bmi is {bmi}
                         </div>
                         : null
                 }
 
+                <div className=" sm:flex hidden justify-center  sm:w-[37.5%] w-fit">
+                    <button className="mt-5 rounded p-2 border-2 border-black hover:bg-black hover:text-white font-semibold" onClick={() => { submit() }}>Calculate</button>
+
+                </div>
+
             </div>
-            <button className="mt-5 rounded p-2 border-2 border-black hover:bg-black hover:text-white font-semibold" onClick={() => { submit() }}>Calculate</button>
         </div>
     )
 
--- src/pages/bmiCalc.js
+++ src/pages/bmiCalc.js
@@ -3,7 +3,7 @@ import Header from "../components/header";
 
 export default function BmiCalc () {
     return(
-        <div className="p-20">
+        <div className="sm:p-20 p-5 pt-20">
             <Header title={"Bmi Calculator"}></Header>
             <BmiForm></BmiForm>
         </div>
